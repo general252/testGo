ef96331eebf4a412c29a6b2b142c28e85d74b7e2
name <name@mail.com>
2020-02-27 12:49:05 +0800

    fix

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/astaxie/beego v1.12.1
+	github.com/general252/testGoMod v0.0.0-20200227044041-fbe4552e3c15
 	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
 )
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/astaxie/beego/logs"
 	"github.com/general252/testGo/pk1/pk2"
+	"github.com/general252/testGoMod/mypkt"
 )
 
 func main() {
 	pk2.MyAdd()
+	mypkt.MySum()
 
 	logs.GetBeeLogger().Info("hello")
 }
