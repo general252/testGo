3486f835e958f3202b15aedd923b0b96826ee9c2
general252 <chuanlin252@163.com>
2020-04-19 16:52:12 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/astaxie/beego/logs"
 	"github.com/general252/testGo/pk1/pk2"
 	"github.com/general252/testGoMod/mypkt"
+	"time"
 )
 
 func main() {
 	pk2.MyAdd()
 	mypkt.MySum()
-
+	
+	for i:=0; i < 10; i ++ {
+		fmt.Printf("time %v\n", time.Now().Unix())
+		time.Sleep(time.Second)
+	}
 	logs.GetBeeLogger().Info("hello")
 }
